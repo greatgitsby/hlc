4cd2a60bb880db0c84ec94d1175f059aa33bc8bc
Trey Moen <treymoen99@gmail.com>
2021-03-27 16:55:15 -0700

    That crap workssssssssssssss

--- src/main/java/com.greatgitsby.hlc/Parser.java
+++ src/main/java/com.greatgitsby.hlc/Parser.java
@@ -12,7 +12,9 @@ public class Parser {
     private final Stack<Symbol> _operandStack;
     private final Stack<Symbol> _operatorStack;
     private final Stack<Symbol> _parseStack;
+
     private Symbol _currentLexerSymbol;
+    private Symbol _currentParserSymbol;
 
     /**
      * Construct a new Parser
@@ -21,42 +23,38 @@ public class Parser {
         _lexicalAnalyzer = lexer;
 
         _currentLexerSymbol = null;
+        _currentParserSymbol = null;
+
         _labelStack = new Stack<>();
         _operandStack = new Stack<>();
         _operatorStack = new Stack<>();
         _parseStack = new Stack<>();
-        _parseTable = new HashMap<>();
+        _parseTable = buildParseTable();
     }
 
     /**
      * TODO Description
      */
-    public boolean isValidSyntax() {
-        ArrayDeque<Symbol> symbolStack;
-        Symbol topOfParseStack;
+    public boolean isValidSyntax() throws SyntaxErrorException {
         boolean isValidSyntax;
 
         isValidSyntax = false;
-        symbolStack = getLexicalAnalyzer().getSymbolStack();
 
         // Push the end symbol ($) onto the parse stack
         getParseStack().push(Token.END_OF_INPUT);
+        getParseStack().push(Token.STATEMENT);
 
-
-
-        while (!symbolStack.isEmpty() && !getParseStack().isEmpty()) {
+        while (!getLexicalAnalyzer().getSymbolStack().isEmpty() && !getParseStack().isEmpty()) {
             // Set the current lexer symbol to the top of the symbol stack
-            setCurrentLexerSymbol(symbolStack.peek());
+            setTopOfLexerStack(getLexicalAnalyzer().getSymbolStack().peek());
 
             // Get the top of parse stack
-            topOfParseStack = getParseStack().peek();
+            setTopOfParseStack(getParseStack().peek());
 
-            // Tell top of parse stack to do its thing
-            topOfParseStack.doTheThing(this);
-
-            // TESTING
-            symbolStack.pop();
+            getTopOfParseStack().doTheThing(this);
         }
+
+        isValidSyntax = true;
 
         return isValidSyntax;
     }
@@ -66,8 +64,12 @@ public class Parser {
      *
      * @return the current lexer symbol
      */
-    public Symbol getCurrentLexerSymbol() {
+    public Symbol getTopOfLexerStack() {
         return _currentLexerSymbol;
+    }
+
+    public Symbol getTopOfParseStack() {
+        return _currentParserSymbol;
     }
 
     /**
@@ -116,9 +118,267 @@ public class Parser {
     }
 
     /**
-     * Sets the current lexer symbol
+     * Sets the new top of lexer stack
+     *
+     * @param theNewTopOfStack the new top of lexer stack
      */
-    private void setCurrentLexerSymbol(Symbol currentSymbol) {
-        _currentLexerSymbol = currentSymbol;
+    private void setTopOfLexerStack(Symbol theNewTopOfStack) {
+        _currentLexerSymbol = theNewTopOfStack;
+    }
+
+    /**
+     * Sets the new top of parse stack
+     *
+     * @param theNewTopOfStack the new top of parse stack
+     */
+    private void setTopOfParseStack(Symbol theNewTopOfStack) {
+        _currentParserSymbol = theNewTopOfStack;
+    }
+
+    private Map<Symbol, Map<Symbol, List<Symbol>>> buildParseTable() {
+        return new HashMap<>() {{
+            put(Token.STATEMENT, new HashMap<>() {{
+
+                put(Token.IDENTIFIER, new LinkedList<>() {{
+                    add(Token.IDENTIFIER);
+                    add(Token.ASSIGNMENT_OP);
+                    add(Token.EXPRESSION);
+                }});
+
+                put(Token.IF, new LinkedList<>() {{
+                    add(Token.IF);
+                    add(Token.BOOLEAN_EXPRESSION);
+                    add(Token.THEN);
+                    add(Token.STATEMENT);
+                    add(Token.ELSE_CLAUSE);
+                }});
+
+                put(Token.WHILE, new LinkedList<>() {{
+                    add(Token.WHILE);
+                    add(Token.BOOLEAN_EXPRESSION);
+                    add(Token.DO);
+                    add(Token.STATEMENT);
+                }});
+
+                put(Token.PRINT, new LinkedList<>() {{
+                    add(Token.PRINT);
+                    add(Token.PRINT_EXPRESSION);
+                }});
+
+                put(Token.BEGIN, new LinkedList<>() {{
+                    add(Token.BEGIN);
+                    add(Token.STATEMENT_LIST);
+                    add(Token.END);
+                }});
+
+                put(Token.VARIABLE, new LinkedList<>() {{
+                    add(Token.VARIABLE);
+                    add(Token.IDENTIFIER);
+                }});
+
+//                put(Token.ELSE, new LinkedList<>());
+//
+//                put(Token.STATEMENT_SEP, new LinkedList<>());
+//
+//                put(Token.END_OF_INPUT, new LinkedList<>());
+//
+//                put(Token.END, new LinkedList<>());
+            }});
+
+            put(Token.ELSE_CLAUSE, new HashMap<>() {{
+
+                put(Token.ELSE, new LinkedList<>() {{
+                    add(Token.ELSE);
+                    add(Token.STATEMENT);
+                }});
+            }});
+
+            put(Token.STATEMENT_LIST, new HashMap<>() {{
+
+                put(Token.IDENTIFIER, new LinkedList<>() {{
+                    add(Token.STATEMENT);
+                    add(Token.SEPARATED_LIST);
+                }});
+
+                put(Token.IF, new LinkedList<>() {{
+                    add(Token.STATEMENT);
+                    add(Token.SEPARATED_LIST);
+                }});
+
+                put(Token.WHILE, new LinkedList<>() {{
+                    add(Token.STATEMENT);
+                    add(Token.SEPARATED_LIST);
+                }});
+
+                put(Token.PRINT, new LinkedList<>() {{
+                    add(Token.STATEMENT);
+                    add(Token.SEPARATED_LIST);
+                }});
+
+                put(Token.BEGIN, new LinkedList<>() {{
+                    add(Token.STATEMENT);
+                    add(Token.SEPARATED_LIST);
+                }});
+
+                put(Token.VARIABLE, new LinkedList<>() {{
+                    add(Token.STATEMENT);
+                    add(Token.SEPARATED_LIST);
+                }});
+
+                put(Token.END, new LinkedList<>());
+            }});
+
+            put(Token.SEPARATED_LIST, new HashMap<>() {{
+
+                put(Token.STATEMENT_SEP, new LinkedList<>() {{
+                    add(Token.STATEMENT_SEP);
+                    add(Token.STATEMENT);
+                    add(Token.SEPARATED_LIST);
+                }});
+            }});
+
+            put(Token.PRINT_EXPRESSION, new HashMap<>() {{
+
+                put(Token.IDENTIFIER, new LinkedList<>() {{
+                    add(Token.EXPRESSION);
+                }});
+
+                put(Token.LEFT_PAREN, new LinkedList<>() {{
+                    add(Token.EXPRESSION);
+                }});
+
+                put(Token.NUMBER, new LinkedList<>() {{
+                    add(Token.EXPRESSION);
+                }});
+
+                put(Token.ADDITIVE_OP, new LinkedList<>() {{
+                    add(Token.EXPRESSION);
+                }});
+
+                put(Token.STRING_CONST, new LinkedList<>() {{
+                    add(Token.STRING_CONST);
+                }});
+            }});
+
+            put(Token.BOOLEAN_EXPRESSION, new HashMap<>() {{
+
+                put(Token.IDENTIFIER, new LinkedList<>() {{
+                    add(Token.EXPRESSION);
+                    add(Token.RELATIONAL_OP);
+                    add(Token.EXPRESSION);
+                }});
+
+                put(Token.LEFT_PAREN, new LinkedList<>() {{
+                    add(Token.EXPRESSION);
+                    add(Token.RELATIONAL_OP);
+                    add(Token.EXPRESSION);
+                }});
+
+                put(Token.NUMBER, new LinkedList<>() {{
+                    add(Token.EXPRESSION);
+                    add(Token.RELATIONAL_OP);
+                    add(Token.EXPRESSION);
+                }});
+
+                put(Token.ADDITIVE_OP, new LinkedList<>() {{
+                    add(Token.EXPRESSION);
+                    add(Token.RELATIONAL_OP);
+                    add(Token.EXPRESSION);
+                }});
+            }});
+
+            put(Token.EXPRESSION, new HashMap<>() {{
+
+                put(Token.IDENTIFIER, new LinkedList<>() {{
+                    add(Token.TERM);
+                    add(Token.ADDITION);
+                }});
+
+                put(Token.LEFT_PAREN, new LinkedList<>() {{
+                    add(Token.TERM);
+                    add(Token.ADDITION);
+                }});
+
+                put(Token.NUMBER, new LinkedList<>() {{
+                    add(Token.TERM);
+                    add(Token.ADDITION);
+                }});
+
+                put(Token.ADDITIVE_OP, new LinkedList<>() {{
+                    add(Token.TERM);
+                    add(Token.ADDITION);
+                }});
+            }});
+
+            put(Token.ADDITION, new HashMap<>() {{
+
+                put(Token.ADDITIVE_OP, new LinkedList<>() {{
+                    add(Token.ADDITIVE_OP);
+                    add(Token.TERM);
+                    add(Token.ADDITION);
+                }});
+            }});
+
+            put(Token.TERM, new HashMap<>() {{
+
+                put(Token.IDENTIFIER, new LinkedList<>() {{
+                    add(Token.FACTOR);
+                    add(Token.MULTIPLICATION);
+                }});
+
+                put(Token.LEFT_PAREN, new LinkedList<>() {{
+                    add(Token.FACTOR);
+                    add(Token.MULTIPLICATION);
+                }});
+
+                put(Token.NUMBER, new LinkedList<>() {{
+                    add(Token.FACTOR);
+                    add(Token.MULTIPLICATION);
+                }});
+
+                put(Token.ADDITIVE_OP, new LinkedList<>() {{
+                    add(Token.FACTOR);
+                    add(Token.MULTIPLICATION);
+                }});
+            }});
+
+            put(Token.MULTIPLICATION, new HashMap<>() {{
+
+                put(Token.MULTIPLICATIVE_OP, new LinkedList<>() {{
+                    add(Token.MULTIPLICATIVE_OP);
+                    add(Token.FACTOR);
+                    add(Token.MULTIPLICATION);
+                }});
+            }});
+
+            put(Token.FACTOR, new HashMap<>() {{
+
+                put(Token.IDENTIFIER, new LinkedList<>() {{
+                    add(Token.IDENTIFIER);
+                }});
+
+                put(Token.LEFT_PAREN, new LinkedList<>() {{
+                    add(Token.LEFT_PAREN);
+                    add(Token.EXPRESSION);
+                    add(Token.RIGHT_PAREN);
+                }});
+
+                put(Token.NUMBER, new LinkedList<>() {{
+                    add(Token.NUMBER);
+                }});
+
+                put(Token.ADDITIVE_OP, new LinkedList<>() {{
+                    add(Token.SIGNED_TERM);
+                }});
+            }});
+
+            put(Token.SIGNED_TERM, new HashMap<>() {{
+
+                put(Token.ADDITIVE_OP, new LinkedList<>() {{
+                    add(Token.ADDITIVE_OP);
+                    add(Token.TERM);
+                }});
+            }});
+        }};
     }
 }
--- src/main/java/com.greatgitsby.hlc/Token.java
+++ src/main/java/com.greatgitsby.hlc/Token.java
@@ -7,6 +7,7 @@ package com.greatgitsby.hlc;
  * language
  */
 public enum Token implements Symbol {
+    // Terminals
     VARIABLE,
     IDENTIFIER,
     NUMBER,
@@ -28,7 +29,107 @@ public enum Token implements Symbol {
     RIGHT_PAREN,
     WHITESPACE,
     COMMENT,
-    END_OF_INPUT;
+    END_OF_INPUT,
+
+    // Non-terminals
+    STATEMENT {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    ELSE_CLAUSE {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    STATEMENT_LIST {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    SEPARATED_LIST {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    PRINT_EXPRESSION {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    BOOLEAN_EXPRESSION {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    EXPRESSION {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    ADDITION {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    TERM {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    MULTIPLICATION {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    FACTOR {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    },
+    SIGNED_TERM {
+        @Override
+        public void doTheThing(Parser theParser) {
+            doTheNonTerminalThing(theParser);
+        }
+    };
+
+    /**
+     * General behavior of non-terminals
+     */
+    protected void doTheNonTerminalThing(Parser theParser) {
+        if (
+            theParser.getParseTable().containsKey(theParser.getTopOfParseStack()) &&
+            theParser.getParseTable().get(theParser.getTopOfParseStack()).containsKey(
+                theParser.getTopOfLexerStack()
+            )
+        ) {
+            theParser.getParseStack().pop();
+
+            for (
+                Symbol s :
+                theParser.getParseTable()
+                    .get(theParser.getTopOfParseStack())
+                    .get(theParser.getTopOfLexerStack())
+            ) {
+                theParser.getParseStack().push(s);
+            }
+        } else {
+            //                throw new SyntaxErrorException("Invalid Syntax");
+            System.err.println("ERROR!!!");
+        }
+    }
 
     /**
      * General behavior for terminals
@@ -36,5 +137,8 @@ public enum Token implements Symbol {
     @Override
     public void doTheThing(Parser theParser) {
         // TODO Implement general behavior
+
+        theParser.getLexicalAnalyzer().getSymbolStack().pop();
+        theParser.getParseStack().pop();
     }
 }
